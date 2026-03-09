e7e1c059b9171850b1fb09b1ed32d694b35de507
Abdoae35 <156617861+Abdoae35@users.noreply.github.com>
2026-03-09 13:47:35 +0200

    Task1Upload

--- lib/features/home/page/home_screen.dart
+++ lib/features/home/page/home_screen.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:greenmart/core/constants/app_assets.dart';
 import 'package:greenmart/core/styles/app_colors.dart';
+import 'package:greenmart/core/styles/text_styles.dart';
 import 'package:greenmart/core/widgets/custom_svg_picture.dart';
+import 'package:greenmart/features/home/data/product_model.dart';
 import 'package:greenmart/features/home/widgets/all_products_builder.dart';
 import 'package:greenmart/features/home/widgets/best_selling_builder.dart';
 import 'package:greenmart/features/home/widgets/exclusive_offer_builder.dart';
 import 'package:greenmart/features/home/widgets/home_search_bar.dart';
+import 'package:greenmart/features/home/widgets/item_card.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -25,7 +28,44 @@ class HomeScreen extends StatelessWidget {
           children: [
             const SizedBox(height: 10),
             HomeSearchBar(),
-            ExclusiveOfferBuilder(),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('Exclusive Offer', style: TextStyles.title),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          'See All',
+                          style: TextStyles.caption1.copyWith(
+                            color: AppColors.primaryColor,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 270,
+                  child: ListView.separated(
+                    padding: const EdgeInsets.symmetric(horizontal: 5),
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      return ItemCard(model: offerProducts[index]);
+                    },
+                    separatorBuilder: (context, index) {
+                      return SizedBox(width: 20);
+                    },
+                    itemCount: offerProducts.length,
+                  ),
+                ),
+              ],
+            ),
+
             BestSellingBuilder(),
             AllProductsBuilder(),
           ],
--- lib/features/home/widgets/exclusive_offer_builder.dart
+++ lib/features/home/widgets/exclusive_offer_builder.dart
@@ -46,5 +46,6 @@ class ExclusiveOfferBuilder extends StatelessWidget {
         ),
       ],
     );
+  
   }
 }
